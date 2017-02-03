cb1ad564ca62a356f43e7a2723cbbe44da3e81fb
amigin <amigin@gmail.com>
2017-02-04 01:31:56 +0200

    Add project files.

---
+++ src/Lykke.SlackNotification.AzureQueue/SlackNotificationViaAzureQueueBinder.cs
@@ -0,0 +1,66 @@
+﻿using Common;
+using Common.Log;
+using Lykke.AzureQueueIntegration;
+using Lykke.AzureQueueIntegration.Publisher;
+using Lykke.SlackNotifications;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Lykke.SlackNotification.AzureQueue
+{
+    public static class SlackNotificationViaAzureQueueBinder
+    {
+
+        public static ISlackNotificationsSender UseSlackNotificationsSender(this IServiceCollection serviceCollection, 
+            AzureQueueSettings settings, ILog log)
+        {
+            var applicationName =
+                Microsoft.Extensions.PlatformAbstractions.PlatformServices.Default.Application.ApplicationName;
+
+            var rabbitMqPublisher = 
+                new AzureQueuePublisher<SlackMessageQueueEntity>(applicationName, settings)
+                .SetLogger(log)
+                .SetSerializer(new SlackNotificationsSerializer())
+                .Start();
+
+            var result = new SlackNotificationsSender(rabbitMqPublisher);
+
+            serviceCollection.AddSingleton<ISlackNotificationsSender>(result);
+
+            return result;
+        }
+    }
+
+
+    public class SlackNotificationsSerializer : IAzureQueueSerializer<SlackMessageQueueEntity>
+    {
+        public string Serialize(SlackMessageQueueEntity model)
+        {
+            return model.ToContract().ToJson();
+        }
+    }
+
+    public class SlackMessageQueueContract
+    {
+        public string Type { get; set; }
+
+        public string Sender { get; set; }
+
+        public string Message { get; set; }
+
+    }
+
+    public static class SlackMessageQueueContractExts
+    {
+        public static SlackMessageQueueContract ToContract(this SlackMessageQueueEntity entity)
+        {
+            return new SlackMessageQueueContract
+            {
+                Type = entity.Type,
+                Message = entity.Message,
+                Sender = entity.Sender
+            };
+        }
+    }
+
+
+}
