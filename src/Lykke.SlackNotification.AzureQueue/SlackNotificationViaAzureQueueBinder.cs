82dfad98749ad571937a5f2abb2401e0c387db04
Sergey Nesterov <sergey.nesterov@lykke.com>
2017-11-29 12:36:12 +0300

    Added SlackNotificationsSender registration via ContainerBuilder

--- src/Lykke.SlackNotification.AzureQueue/SlackNotificationViaAzureQueueBinder.cs
+++ src/Lykke.SlackNotification.AzureQueue/SlackNotificationViaAzureQueueBinder.cs
@@ -5,13 +5,16 @@ using Lykke.AzureQueueIntegration.Publisher;
 using Lykke.SlackNotifications;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.PlatformAbstractions;
+using Autofac;
 
 namespace Lykke.SlackNotification.AzureQueue
 {
     public static class SlackNotificationViaAzureQueueBinder
     {
-        public static ISlackNotificationsSender UseSlackNotificationsSenderViaAzureQueue(this IServiceCollection serviceCollection, 
-            AzureQueueSettings settings, ILog log = null)
+        public static ISlackNotificationsSender UseSlackNotificationsSenderViaAzureQueue(
+            this IServiceCollection serviceCollection, 
+            AzureQueueSettings settings,
+            ILog log = null)
         {
             var applicationName = PlatformServices.Default.Application.ApplicationName;
 
@@ -24,6 +27,26 @@ namespace Lykke.SlackNotification.AzureQueue
             var result = new SlackNotificationsSender(azureQueuePublisher);
 
             serviceCollection.AddSingleton<ISlackNotificationsSender>(result);
+
+            return result;
+        }
+
+        public static ISlackNotificationsSender UseSlackNotificationsSenderViaAzureQueue(
+            this ContainerBuilder containerBuilder,
+            AzureQueueSettings settings,
+            ILog log = null)
+        {
+            var applicationName = PlatformServices.Default.Application.ApplicationName;
+
+            var azureQueuePublisher =
+                new AzureQueuePublisher<SlackMessageQueueEntity>(applicationName, settings)
+                .SetLogger(log)
+                .SetSerializer(new SlackNotificationsSerializer())
+                .Start();
+
+            var result = new SlackNotificationsSender(azureQueuePublisher);
+
+            containerBuilder.RegisterInstance(result).As<ISlackNotificationsSender>().SingleInstance();
 
             return result;
         }
