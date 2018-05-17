734b1b36f99b1294110e38971a99b4a46c778341
Sergey Nesterov <sergey.nesterov@lykke.com>
2018-05-17 12:02:40 +0300

    Added additional check for queue message size

--- src/Lykke.SlackNotification.AzureQueue/SlackMessageQueueContractExts.cs
+++ src/Lykke.SlackNotification.AzureQueue/SlackMessageQueueContractExts.cs
@@ -2,16 +2,39 @@
 
 namespace Lykke.SlackNotification.AzureQueue
 {
+    /// <summary>
+    /// Extensions for SlackMessageQueueEntity conversion to SlackMessageQueueContract
+    /// </summary>
     public static class SlackMessageQueueContractExts
     {
+        private const int _kb = 1024;
+        // 48 Kb - max base64 encoded Azure queue message size
+        private const int _maxMessageBese64SizeInKb = 48;
+
+        /// <summary>
+        /// Default conversion SlackMessageQueueEntity to SlackMessageQueueContract
+        /// </summary>
+        /// <param name="entity">Entity to be converted</param>
+        /// <returns>Entity converted to SlackMessageQueueContract</returns>
         public static SlackMessageQueueContract ToContract(this SlackMessageQueueEntity entity)
         {
-            // 48 Kb - max base64 encoded Azure queue message size
-            // 1 Kb - reserved for json format, model.Sender, model.Type and message headers
-            const int kb = 1024;
-            const int maxLength = 48 * kb - 1 * kb;
+            return ToContract(entity, 0);
+        }
 
-            var message = entity.Message.Length > maxLength ? entity.Message.Substring(0, maxLength) : entity.Message;
+        /// <summary>
+        /// Conversion SlackMessageQueueEntity to SlackMessageQueueContractwith regard of message size overhead
+        /// </summary>
+        /// <param name="entity">Entity to be converted</param>
+        /// <param name="overheadSymbolsCount">Overhead size</param>
+        /// <returns>Entity converted to SlackMessageQueueContract with possibly resized message</returns>
+        public static SlackMessageQueueContract ToContract(this SlackMessageQueueEntity entity, int overheadSymbolsCount)
+        {
+            // 1 Kb - reserved for json format, model.Sender, model.Type and message headers
+            int maxMessageLength = (_maxMessageBese64SizeInKb - 1) * _kb - overheadSymbolsCount;
+
+            var message = entity.Message.Length > maxMessageLength
+                ? $"{entity.Message.Substring(0, maxMessageLength - 3)}..."
+                : entity.Message;
 
             return new SlackMessageQueueContract
             {
--- src/Lykke.SlackNotification.AzureQueue/SlackNotificationsSerializer.cs
+++ src/Lykke.SlackNotification.AzureQueue/SlackNotificationsSerializer.cs
@@ -4,11 +4,32 @@ using Lykke.SlackNotifications;
 
 namespace Lykke.SlackNotification.AzureQueue
 {
+    /// <summary>
+    /// Serializer for SlackMessageQueueEntity
+    /// </summary>
     public class SlackNotificationsSerializer : IAzureQueueSerializer<SlackMessageQueueEntity>
     {
+        private const int _maxQueueMessageBase64Size = 49152; //48 Kb
+
+        /// <summary>
+        /// Converts SlackMessageQueueEntity to string with size within base64 encoded max message length range
+        /// </summary>
+        /// <param name="model">Model to be serialized</param>
+        /// <returns>Serialized string</returns>
         public string Serialize(SlackMessageQueueEntity model)
         {
-            return model.ToContract().ToJson();
+            string result = model.ToContract().ToJson();
+            if (result.Length > _maxQueueMessageBase64Size)
+            {
+                int overhead = result.Length - _maxQueueMessageBase64Size;
+                do
+                {
+                    result = model.ToContract(overhead).ToJson();
+                    overhead *= 2;
+                }
+                while (result.Length > _maxQueueMessageBase64Size);
+            }
+            return result;
         }
     }
 }
